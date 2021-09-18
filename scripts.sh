4b425461335f4fc4d3a4bf201a8a6565273e7490
赵悦男 <2777968262@qq.com>
2021-09-18 20:26:52 +0800

    scripts: Add note
    添加注意事项
    
    Signed-off-by: 赵悦男 <2777968262@qq.com>
    Change-Id: I3f909c33fe69310b5dc7b493edeea4cc78a9ecac

--- scripts.sh
+++ scripts.sh
@@ -12,3 +12,12 @@
 #       the names of its contributors may be used to endorse or promote
 #       products derived from this software without specific prior written
 #       permission.
+#
+# 该脚本只是为了 "尽可能" 的让官方内核也拥有些三方的内核更改优化
+# 受限于内核与脚本的区别, 只能实现些微小的更改, 如果想获得大幅度提升,
+# 还是建议您刷入第三方内核.
+#
+#   针对设备的注意事项:
+#  因为用户大部分喜欢刷官改包, 官改包基本都是解密data状态,
+#  所以您需要手动将脚本中的 "dm-5" 的字样替换为 "sda34" .
+#
